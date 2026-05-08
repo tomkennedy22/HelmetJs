642ce5ac7a630c39921011df24957bfe3e14b919
Thomas Kennedy <thomasconnorkennedy@gmail.com>
2026-05-08 10:02:40 -0500

    fixes

--- src/common.ts
+++ src/common.ts
@@ -1,8 +1,8 @@
 export const helmetStyles = [
-  "Standard",
-  "Tiger Stripe",
-  "Wing",
-  "Horn",
+	"Standard",
+	"Tiger Stripe",
+	"Wing",
+	"Horn",
 ] as const;
 const baseColor = "#f00";
 const darkColor = "#272727";
@@ -10,27 +10,27 @@ const darkColor = "#272727";
 type HelmetStyle = (typeof helmetStyles)[number];
 
 type HelmetConfigBase = {
-  flipHelmet: boolean;
-  helmetColor: string;
-  facemaskColor: string;
-  helmetLogoUrl?: string;
-  xAdjust: number;
-  yAdjust: number;
-  helmetLogoScale: number;
-  enableLogo: boolean;
-  useFlippedLogoUrlWhenFlipped: boolean;
-  flipLogoWithHelmet: boolean;
-  flippedHelmetLogoUrl?: string;
-  enableHelmetStickers: boolean;
-  helmetStickerUrl?: string;
-  numHelmetStickers?: number;
+	flipHelmet: boolean;
+	helmetColor: string;
+	facemaskColor: string;
+	helmetLogoUrl?: string;
+	xAdjust: number;
+	yAdjust: number;
+	helmetLogoScale: number;
+	enableLogo: boolean;
+	useFlippedLogoUrlWhenFlipped: boolean;
+	flipLogoWithHelmet: boolean;
+	flippedHelmetLogoUrl?: string;
+	enableHelmetStickers: boolean;
+	helmetStickerUrl?: string;
+	numHelmetStickers?: number;
 };
 
 type HelmetStyleConfig = {
-  helmetStyle?: HelmetStyle;
-  tigerStripeColor?: string;
-  wingColor?: string;
-  hornColor?: string;
+	helmetStyle?: HelmetStyle;
+	tigerStripeColor?: string;
+	wingColor?: string;
+	hornColor?: string;
 };
 
 export type HelmetConfig = HelmetConfigBase & HelmetStyleConfig;
@@ -38,32 +38,37 @@ export type HelmetConfig = HelmetConfigBase & HelmetStyleConfig;
 export type HelmetConfigOverrides = Partial<HelmetConfig>;
 
 export const generateHelmetConfigFromOverrides = ({
-  helmetConfigOverrides,
+	helmetConfigOverrides,
 }: {
-  helmetConfigOverrides: HelmetConfigOverrides;
+	helmetConfigOverrides: HelmetConfigOverrides;
 }): HelmetConfig => {
-  const helmetConfig: HelmetConfig = {
-    flipHelmet: helmetConfigOverrides?.flipHelmet || false,
-    helmetColor: helmetConfigOverrides?.helmetColor || baseColor,
-    facemaskColor: helmetConfigOverrides?.facemaskColor || darkColor,
-    helmetLogoUrl: helmetConfigOverrides?.helmetLogoUrl || undefined,
-    xAdjust: helmetConfigOverrides?.xAdjust || 0,
-    yAdjust: helmetConfigOverrides?.yAdjust || 0,
-    enableLogo: helmetConfigOverrides?.enableLogo || false,
-    flipLogoWithHelmet: helmetConfigOverrides?.flipLogoWithHelmet || false,
-    useFlippedLogoUrlWhenFlipped:
-      helmetConfigOverrides?.useFlippedLogoUrlWhenFlipped || false,
-    flippedHelmetLogoUrl:
-      helmetConfigOverrides?.flippedHelmetLogoUrl || undefined,
-    helmetStyle: helmetConfigOverrides?.helmetStyle || "Standard",
-    tigerStripeColor: helmetConfigOverrides?.tigerStripeColor || darkColor,
-    wingColor: helmetConfigOverrides?.wingColor || darkColor,
-    hornColor: helmetConfigOverrides?.hornColor || darkColor,
-    helmetLogoScale: helmetConfigOverrides?.helmetLogoScale || 1,
-    enableHelmetStickers: helmetConfigOverrides?.enableHelmetStickers || false,
-    helmetStickerUrl: helmetConfigOverrides?.helmetStickerUrl || undefined,
-    numHelmetStickers: helmetConfigOverrides?.numHelmetStickers || 5,
-  };
+	const helmetConfig: HelmetConfig = {
+		flipHelmet: helmetConfigOverrides?.flipHelmet || false,
+		helmetColor: helmetConfigOverrides?.helmetColor || baseColor,
+		facemaskColor: helmetConfigOverrides?.facemaskColor || darkColor,
+		helmetLogoUrl: helmetConfigOverrides?.helmetLogoUrl || undefined,
+		xAdjust: helmetConfigOverrides?.xAdjust || 0,
+		yAdjust: helmetConfigOverrides?.yAdjust || 0,
+		enableLogo: helmetConfigOverrides?.enableLogo || false,
+		flipLogoWithHelmet: helmetConfigOverrides?.flipLogoWithHelmet || false,
+		useFlippedLogoUrlWhenFlipped:
+			helmetConfigOverrides?.useFlippedLogoUrlWhenFlipped || false,
+		flippedHelmetLogoUrl:
+			helmetConfigOverrides?.flippedHelmetLogoUrl || undefined,
+		helmetStyle: helmetConfigOverrides?.helmetStyle || "Standard",
+		tigerStripeColor: helmetConfigOverrides?.tigerStripeColor || darkColor,
+		wingColor: helmetConfigOverrides?.wingColor || darkColor,
+		hornColor: helmetConfigOverrides?.hornColor || darkColor,
+		helmetLogoScale: helmetConfigOverrides?.helmetLogoScale || 1,
+		enableHelmetStickers: helmetConfigOverrides?.enableHelmetStickers || false,
+		helmetStickerUrl: helmetConfigOverrides?.helmetStickerUrl || undefined,
+		numHelmetStickers: helmetConfigOverrides?.numHelmetStickers || 5,
+	};
 
-  return helmetConfig;
+	return helmetConfig;
+};
+
+export const roundDecimal = (num: number, decimalPlaces: number): number => {
+	const factor = 10 ** decimalPlaces;
+	return Math.round(num * factor) / factor;
 };
